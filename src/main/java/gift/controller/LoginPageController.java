18fa2877c742e259e849cc1d00d7dd11a1922b60
hardjava <fkdlcn123@kumoh.ac.kr>
2025-07-26 18:40:33 +0900

    refactor: 카카오 로그인 URL UriComponentsBuilder로 생성

--- src/main/java/gift/controller/LoginPageController.java
+++ src/main/java/gift/controller/LoginPageController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Controller
 @RequestMapping("/login")
@@ -17,13 +20,15 @@ public class LoginPageController {
 
     @GetMapping("/page")
     public String loginPage(Model model) {
-        String url = String.format(
-                "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s",
-                kakaoOauthConfig.clientId(),
-                kakaoOauthConfig.redirectUri()
-        );
+        URI uri =
+                UriComponentsBuilder.fromUriString("https://kauth.kakao.com")
+                        .path("oauth/authorize")
+                        .queryParam("response_type", "code")
+                        .queryParam("client_id", kakaoOauthConfig.clientId())
+                        .queryParam("redirect_uri", kakaoOauthConfig.redirectUri())
+                        .build().toUri();
 
-        model.addAttribute("kakaoLoginUrl", url);
+        model.addAttribute("kakaoLoginUrl", uri);
         return "login-page";
     }
 }
